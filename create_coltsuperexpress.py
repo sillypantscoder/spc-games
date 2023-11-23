c56df5b93e07e6a2b57518d3c40a8e84cd480032
sillypantscoder <funjasonroman@gmail.com>
2023-11-23 12:13:42 -0700

    Better error handling
    Still need to be able to leave the game in the voting game

--- create_coltsuperexpress.py
+++ create_coltsuperexpress.py
@@ -6,7 +6,7 @@ f = open("coltsuperexpress/public_files/script.js", "r")
 t = f.read().split("\n")
 f.close()
 
-t[38] = r'		x.send(body.replaceAll("\n", "\\n"))'
+t[434] = r'		container.children[0].innerHTML = `<h3>Lost connection with the server</h3>`; container.appendChild(document.createElement("div")); container.children[1].innerHTML = `<button onclick="location.assign(\`/\`)">Exit this game</button>`'
 
 f = open("coltsuperexpress/public_files/script.js", "w")
 f.write("\n".join(t))
